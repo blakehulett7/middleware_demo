e0f7e8cb34c8cd4016026ae79bda68099b5a5554
blakehulett7 <blake.hulett7@gmail.com>
2025-05-16 09:06:48 -0500

    baseline works

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Jesus is Lord!")
+
+	router := http.NewServeMux()
+	router.HandleFunc("/middleware-demo", handle_test)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	server.ListenAndServe()
+}
+
+func handle_test(w http.ResponseWriter, r *http.Request) {
+	res := struct {
+		Res string `json:"res"`
+	}{"Jesus is Lord!"}
+	data, _ := json.Marshal(res)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(data)
 }
